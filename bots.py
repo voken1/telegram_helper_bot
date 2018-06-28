e98fdfa9970423bd510d9853b5c4188cfa78fa7d
lymansnow <vnlymansnow@gmail.com>
2018-06-28 14:01:30 +0000

    update bots.py

--- bots.py
+++ bots.py
@@ -9,7 +9,4 @@ for bot in conf.bots:
     print('Starting BOT:', bot['token'].split(':')[0])
     bots.append(TgBot(token=bot['token'], lang=bot['lang']))
 
-a = bots[0]
-
-
 vn_console.embed(banner='Listening....')
